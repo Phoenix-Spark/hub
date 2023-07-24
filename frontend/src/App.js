486eb1e255fd7d7d1adb540d031fba7798075e46
DavidJohnFlood <David.John.Flood@gmail.com>
2023-07-24 15:44:10 +0900

    Basic Frontend, add server README.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+import { useState, useEffect, createContext } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header, Hub, Cell, Project } from './pages/index.js';
+export const AppContext = createContext();
+
+const server = process.env.REACT_APP_SERVER_STRING || "http://localhost:8080";
+
+export default function App() {
+  const [user, setUser] = useState({})
+  const [sparkList, setSparkList] = useState([])
+
+  const ContextObject = { server, user, setUser, sparkList}
+
+  useEffect(() => {
+    fetch(`${server}/spark_list`)
+        .then(res=>{console.log(res);
+                    return res.json();})
+        .then(data=>setSparkList(data))
+        .catch(err=>console.log(`Fetch failed. Error: ${err}`))
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="AppWrapper">
+      <AppContext.Provider value={ContextObject}>
+        <BrowserRouter>
+          <div id="HeaderContainer"><Header /></div>
+          <div id="BodyContainer">
+            <Routes>
+              <Route path='/' element={<Hub />} />
+              <Route path='/cell/*' element={<Cell />} />
+              <Route path='/project/*' element={<Project />} />
+              <Route path='/*' element={<Hub />} /> {/*catch all*/}
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AppContext.Provider>
     </div>
   );
 }
-
-export default App;
---
+++ frontend/src/pages/Cell.js
@@ -0,0 +1,27 @@
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppContext } from '../App.js'
+import { useState, useEffect, useContext } from 'react';
+
+export default function Cell() {
+    const { server } = useContext(AppContext);
+    const [cellData, setCellData] = useState({})
+
+    let cellId = 1
+
+    useEffect(() => {
+        fetch(`${server}/cell/${cellId}`)
+            .then(res=>{console.log(res);
+                        return res.json();})
+            .then(data=>setCellData(data[0]))
+            .catch(err=>console.log(`Fetch failed. Error: ${err}`))
+    }, [server, cellId]);
+
+    return (
+        <>
+            <div>This is the Cell Component!</div>
+            <br/>
+            {JSON.stringify(cellData)}
+        </>
+    )
+}
