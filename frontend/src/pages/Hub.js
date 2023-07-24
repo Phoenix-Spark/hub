07df85a0a6da8cd70dca297040a86ef560b9ce1b
Sugarfreeboi <laurence.portraitart@gmail.com>
2023-07-24 10:56:47 -0700

    resolving unbranched conflicts

--- frontend/src/pages/Hub.js
+++ frontend/src/pages/Hub.js
@@ -1,16 +1,52 @@
+import React from 'react';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppContext from '../AppContext.js';
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 
 export default function Hub() {
-    const { sparkList } = useContext(AppContext);
+  const { sparkList } = useContext(AppContext);
+  const [searchQuery, setSearchQuery] = useState('');
 
-    return (
-        <>
-            <div>This is the Hub Component!</div>
-            <br/>
-            {JSON.stringify(sparkList)}
-        </>
-    )
+  const handleSearchInputChange = event => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredSparkList = sparkList.filter(spark => spark.toLowerCase().includes(searchQuery.toLowerCase()));
+
+  return (
+    <div className="container mt-5">
+      <h3 className="mb-4">This is the Hub Component!</h3>
+      
+      <div className="mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search for..."
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+        />
+      </div>
+
+      <div className="row">
+        <div className="col-md-8">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Spark List</h5>
+              <ul className="list-group">
+                {filteredSparkList.map((spark, index) => (
+                  <li
+                    className="list-group-item"
+                    key={index}
+                  >
+                    {spark}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
