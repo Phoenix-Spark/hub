bae1d81824f0e762266173cb1858fd025f9d557c
FlippantPope <jonpmiddleton@gmail.com>
2023-08-02 19:31:58 -0700

    changed cors

--- server/src/app.js
+++ server/src/app.js
@@ -36,7 +36,7 @@ const sessionOptions = {
 };
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://capstone.staging.apps.techpulse.us', 'https://*.jmidd.dev'],
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', /\*.techpulse.us/, /\*.jmidd.dev/],
   credentials: true,
 };
 
